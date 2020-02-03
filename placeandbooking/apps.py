79125b24d9e267d6eff2fa723ba4a109036e0804
majidGMZ <majid.gmz77@gmail.com>
2020-02-03 23:35:16 +0330

    application files added

---
+++ placeandbooking/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PlaceandbookingConfig(AppConfig):
+    name = 'placeandbooking'
---
+++ placeandbooking/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    #path('new_place/',views.CreatPlace.as_view(),name = 'newplace')
+]
