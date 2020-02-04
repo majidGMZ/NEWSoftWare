50d345d913f729b0c665dac356aeaa879bdc44d5
majidGMZ <majid.gmz77@gmail.com>
2020-02-04 16:18:42 +0330

    2 models created 1.place 2.booking maybe there are changes in futher

--- placeandbooking/urls.py
+++ placeandbooking/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'placeandbooking'
 
 urlpatterns = [
-    path('new_place/',views.CreatPlace.as_view(),name = 'newplace')
+    path('new_place/',views.CreatPlace.as_view(),name = 'newplace'),
+    path('places/',views.IndexPlace.as_view(),name = 'places'),
+    path('places/<int:pk>/',views.PlaceDetail.as_view(),name = 'place_detail'),
 ]
--- placeandbooking/views.py
+++ placeandbooking/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
+
+
 from placeandbooking import models
 
 
@@ -20,3 +22,18 @@ class CreatPlace(generic.CreateView):
               'SmokingAllowed',
               ]
 
+class IndexPlace(generic.ListView):
+    models = models.Place
+    template_name = 'placeandbooking/places_to_rent.html'
+    context_object_name = 'places'
+
+    def get_queryset(self):
+        return models.Place.objects.all()
+
+class PlaceDetail(generic.DeleteView):
+    models = models.Place
+    template_name = 'placeandbooking/place_detail.html'
+    context_object_name = 'place_detail'
+
+    def get_queryset(self):
+        return models.Place.objects.all();
