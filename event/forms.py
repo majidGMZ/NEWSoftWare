e037164de88be5175915ee65c0293ee926b1cf11
faezeh <faezehfarajzade@gmail.com>
2020-02-04 12:00:31 -0800

    event(1)

--- event/forms.py
+++ event/forms.py
@@ -1,17 +1,18 @@
 from django import forms
-from .models import Events
+from .models import Events, Comment
 
 
 class EventModelForm(forms.ModelForm):
     class Meta:
         model = Events
         fields = [
-            'username',
-            'title',
-            'text',
-            'city',
-            'date_from',
-            'date_to',
-            'clock_from',
-            'clock_to',
+            'username', 'title', 'text', 'city', 'date_from', 'date_to', 'clock_from', 'clock_to',
+        ]
+
+
+class CommentModelForm(forms.ModelForm):
+    class Meta():
+        model = Comment
+        fields = [
+            'username', 'text',
         ]
--- event/urls.py
+++ event/urls.py
@@ -1,14 +1,18 @@
-from django.urls import path,re_path
+from django.urls import path, re_path
 from event import views
 
 app_name = "event"
 
-
-
-
 urlpatterns = [
     path('', views.EventListView, name='list_event'),
     path('<int:pk>/', views.EventDetailView.as_view(), name='detail'),
-    path('s/<int:pk>/',views.CommentOneEventDetailView.as_view(),name= 'comment_detail'),
-    path('create/',views.EventCreateView.as_view(),name='event_create')
+    path('s/<int:pk>/', views.CommentOneEventDetailView.as_view(), name='comment_detail'),
+    path('create/', views.EventCreateView.as_view(), name='event_create'),
+    path('<int:pk>/update/', views.EventUpdateView.as_view(), name='event_update'),
+    path('home/search/', views.SearchResultsView.as_view(), name='event_search'),
+    path('home/', views.HomePageView.as_view(), name='home'),
+    path('home/search/<int:pk>/', views.EventDetailView.as_view(), name='detail_search'),
+    path('<int:id>/remove/', views.EventDeleteView.as_view(), name='event_delete'),
+    path('<int:pk>/comment/', views.add_comment_to_post(), name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve(), name='comment_approve'),
 ]
