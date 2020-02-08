45781aef436c957fba22e506630d635ee206d00f
faezeh <faezehfarajzade@gmail.com>
2020-02-08 14:46:43 -0800

    event(3)

--- SoftWare/urls.py
+++ SoftWare/urls.py
@@ -22,5 +22,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('event/', include('event.urls')),
+    path('', include('event.urls')),
 ]
--- event/forms.py
+++ event/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Events, Comment
+from .models import Events, Comment, Discussion, CommentDiscussion
 
 
 class EventModelForm(forms.ModelForm):
@@ -13,6 +13,25 @@ class EventModelForm(forms.ModelForm):
 class CommentModelForm(forms.ModelForm):
     class Meta:
         model = Comment
+        fields = [
+            'username', 'text',
+        ]
+
+
+########discussion######
+
+
+class DiscussionModelForm(forms.ModelForm):
+    class Meta:
+        model = Discussion
+        fields = [
+            'username', 'title', 'text', 'city',
+        ]
+
+
+class CommentDiscussionModelForm(forms.ModelForm):
+    class Meta:
+        model = CommentDiscussion
         fields = [
             'username', 'text',
         ]
