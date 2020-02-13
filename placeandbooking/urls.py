2deee8323168a8f7ac63afd5264dadb8559148ef
majidGMZ <majid.gmz77@gmail.com>
2020-02-13 19:25:59 +0330

    Place and booking Backend compeleted

--- placeandbooking/urls.py
+++ placeandbooking/urls.py
@@ -14,9 +14,11 @@ urlpatterns = [
     path('search/<int:pk>/Book',views.RegisterBooking.as_view(),name = 'send_request'),
     path('bookings/',views.BookingList.as_view(), name = 'bookings'),
     path('bookings/<int:pk>/',views.BookingDetail.as_view(), name = 'booking_detail'),
-    path('placesbooking/',views.BookingsPlaceList.as_view(), name = 'place_booking'),
-    path('placesbooking/<int:pk>/',views.BookingForPlace.as_view(),name = 'booking_for_place'),
-    path('placesbooking/<int:place_id>/<int:pk>/',views.BookingDetailPlace.as_view(), name = 'book_detail'),
+    path('mybooking/',views.mybookings.as_view(), name = 'my_bookings'),
+    path('mybooking/<int:pk>',views.mybookingdetail.as_view(),name = 'my_booking_detail'),
     path('update/<int:pk>/',views.HostAccept.as_view(),name = 'acceptance'),
     path('delete/<int:pk>/',views.HostDeleteBooking.as_view(), name = 'reject'),
+    path('myplace/',views.IndexPlace_for_person.as_view(),name = 'person_place'),
+    path('myplace/<int:pk>',views.PlaceDetail_for_person.as_view(),name = 'person_place_detail'),
+    path('myplace/<int:pk>/update/',views.EditPlace.as_view(), name = 'edit_place')
 ]
